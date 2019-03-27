959f8c0722463f1400e8eefac2460b186a3d39c4
CyberVGamer <vallen813@gmail.com>
2019-03-26 18:05:03 -0700

    MouseInput
    Working on MouseInput script

---
+++ WitchGame/Assets/Project/Scripts/MouseController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MouseController : MonoBehaviour
+{
+    //VARIABLES
+
+    //Misc
+    public Camera mainCam;
+    public Vector2 mousePos;
+    //public ButtonAction action;
+
+    //Arrays
+    public BoxCollider2D[] buttons = new BoxCollider2D[3];
+
+    //Booleans
+    bool hoveredLastFrame;
+    bool clickedLastFrame;
+
+
+    //FUNCTIONS
+
+    private void Start()
+    {
+        for(int i = 0; i <= buttons.Length; i ++)
+        {
+            if (!buttons[i]) buttons[i] = GetComponent<BoxCollider2D>();
+        }
+    }
+    /*
+    * script checks array of colliders every frame, if mouse position hovers over collider in array,
+    * script grabs information associated with collider
+    */
+
+    protected virtual void Update()
+    {
+        for(int i = 0; i <= buttons.Length; i++)
+        {
+            if (buttons[i].OverlapPoint(mainCam.ScreenToWorldPoint(Input.mousePosition)))
+            {
+                hoveredLastFrame = true;
+                OnHover();
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    OnClick();
+                }
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    clickedLastFrame = true;
+                    OnClickHeld();
+                }
+                else if (clickedLastFrame)
+                {
+                    clickedLastFrame = false;
+                    OnClickExit();
+                }
+            }
+            else if (hoveredLastFrame)
+            {
+                hoveredLastFrame = false;
+                OnHoverExit();
+                clickedLastFrame = false;
+                OnClickExit();
+            }
+        }
+    }
+
+    protected virtual void OnClick()
+    {
+    //    action.Exuecute();
+    }
+
+    protected abstract void OnHover();
+    protected abstract void OnClickHeld();
+    protected abstract void OnClickExit();
+    protected abstract void OnHoverExit();
+}
---
+++ WitchGame/Assets/Project/Scripts/Toolkit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Toolkit
+{
+    
+}
+
+public class Timer
+{
+    // Floats
+    public float t;     //current time
+    public float sp;    //speed
+
+    //Booleans
+    public bool tog;    //toggle
+
+    /// <summary>
+    /// Constructor for the Timer script, define your variables here homie
+    /// </summary>
+    /// <param name="speed">Speed of the timer, use 1 for seconds</param>
+    /// <param name="toggle">toggles the timer on and off</param>
+    public Timer(float speed, bool toggle)
+    {
+        sp = speed;
+        tog = toggle;
+        t = 0;
+    }
+
+    /// <summary>
+    /// Runs the timer script
+    /// </summary>
+    /// <param name="deltaTime">time since last update</param>
+    public void Update(float deltaTime)
+    {
+        //if tog is true, start adding deltatime multiplied by sp (speed) to t
+        if(tog) t += deltaTime * sp;
+    }
+
+    /// <summary>
+    /// Restarts the timer, sets t equal to 0
+    /// </summary>
+    public void Restart()
+    {
+        //resets t to 0
+        t = 0;
+    }
+}
