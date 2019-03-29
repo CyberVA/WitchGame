4dcaeb2a14d46747da65887834bdad0806e24881
CyberVGamer <vallen813@gmail.com>
2019-03-29 10:15:17 -0700

    MouseInput Base
    Finished the base scripts for the mouse input system

---
+++ WitchGame/Assets/Project/Scripts/UI/MouseController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    //VARIABLES
+
+    //Misc
+    public Camera mainCam;
+    public Vector2 mousePos;
+
+    //Arrays
+    public Clickable[] clickables = new Clickable[3];
+
+    //Booleans
+    bool hoveredLastFrame;
+    bool clickedLastFrame;
+
+
+    //FUNCTIONS
+
+    private void Start()
+    {
+        for(int i = 0; i <= clickables.Length; i ++)
+        {
+            if (!clickables[i]) clickables[i] = GetComponent<Clickable>();
+        }
+    }
+    /*
+    * script checks array of colliders every frame, if mouse position hovers over collider in array,
+    * script grabs information associated with collider
+    */
+
+    protected virtual void Update()
+    {
+        for(int i = 0; i <= clickables.Length; i++)
+        {
+            if (clickables[i].myBox.OverlapPoint(mainCam.ScreenToWorldPoint(Input.mousePosition)))
+            {
+                clickables[i].OnHover();
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    clickables[i].OnClick();
+                }
+            }
+        }
+    }
+
+    protected virtual void OnClick()
+    {
+
+    }
+}
---
+++ WitchGame/Assets/Project/Scripts/UI/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : Clickable
+{
+
+    public string[] scenes = new string[2];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scenes[1] = "Project/Scenes/Official";
+        scenes[2] = "Project/scenes/Vincent";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnClick()
+    {
+        SceneManager.LoadScene(scenes[1]);
+    }
+}
