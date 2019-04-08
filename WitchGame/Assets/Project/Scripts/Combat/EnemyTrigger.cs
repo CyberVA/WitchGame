474c752f43ab343cecf100e541da14942d41c95c
Eli <elijahdsummy@gmail.com>
2019-04-08 13:37:10 -0700

    Combat prep

---
+++ WitchGame/Assets/Project/Scripts/Combat/EnemyTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwoStepCollision;
+
+public enum TriggerType {None, PlayerShroom, ShroomPunch }
+
+public class Trigger
+{
+    public Box bounds;
+
+    public TriggerType type;
+    public int otherData;
+}
---
+++ WitchGame/Assets/Project/Scripts/GlobalData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GlobalData : MonoBehaviour
+{
+    public static GlobalData instance;
+
+    /// <summary>
+    /// Triggers that enemies need to check
+    /// </summary>
+    public IEnumerable<Trigger> enemyTriggers;
+    /// <summary>
+    /// Triggers that player needs to check
+    /// </summary>
+    public IEnumerable<Trigger> playerTriggers;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
