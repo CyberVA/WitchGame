20661439d6e767ee0e9b58b2e813b728ff5687fa
Julia Pickett <jkpdragonlover@gmail.com>
2019-04-15 13:37:31 -0700

    SOMEANIMATIONSANDSH-T
    HEY GOTTA GO

--- WitchGame/Assets/Project/Scripts/UI/Nodes/Clickable_RebindingNode.cs
+++ WitchGame/Assets/Project/Scripts/UI/Nodes/Clickable_RebindingNode.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Clickable_RebindingNode : Clickable
 {
 
+    public string keyToChange;
     public Color highlightColor;
-    //public whatami- to dictate what keybind the button is currently assgined to change - just- Figure It Out
-
 
     private KeyCode newKey;
 
-    private bool waitingForKey;
+    private bool waitingForKey = false;
 
 
     public override void OnClick()
     {
-        //dothatstuffdude
+        //TODO:
+        //Enter a state where it starts looking for the next key press
+        //when that (the key press) happens, assign the last key pressed to newKey
+        //change the newKey to be the keycode used for the specified direction
+        //exit the state of waiting for a keypress
+
+        //the problem: how to get each button to recognize what it needs to change w/out cheesing it
+        //InputManager.IM.(keyToChange) = newKey; doesn't work
+        //heres the cheese: a string, and a switch on that sting. depending on the string the button has been assigned it changes the whatever blah blah AWFUL i'm doing it
     }
 
     public override void OnHover()
