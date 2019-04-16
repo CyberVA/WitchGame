ddaec46f35cee4cd0197f4db3006ad70bed3fc39
Julia <jkpdragonlover@gmail.com>
2019-04-16 12:55:06 -0700

    blarg
    the buttons will all only work if you enter the scenes from the main menu (so you can grab the GM)
    credits sh-t, along with uuuuh the win- screen. which is a scene.

---
+++ WitchGame/Assets/Project/Scripts/UI/Nodes/Clickable_ToggleNode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable_ToggleNode : Clickable
+{
+    public Color highlightColor;
+    public GameObject tog;
+    bool toggle = true;
+
+    public override void OnClick()
+    {
+        SpriteRenderer sr;
+        sr = tog.GetComponent<SpriteRenderer>();
+        if (toggle == true)
+        {
+            //Debug.Log("i got pressed as true and am now turning false");
+            sr.enabled = true;
+            toggle = false;
+        }
+        else
+        {
+            //Debug.Log("i got pressed as false and am now turning true");
+            sr.enabled = false;
+            toggle = true;
+        }
+    }
+
+    public override void OnHover()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = highlightColor;
+    }
+
+    public override void OnEmpty()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
