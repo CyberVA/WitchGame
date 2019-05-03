e35f4551f87acb2deed3864d6612ab3ba6a8ff28
CyberVGamer <vallen813@gmail.com>
2019-05-03 13:27:24 -0700

    pause
    more paused

--- WitchGame/Assets/Project/Scripts/UI/Pause.cs
+++ WitchGame/Assets/Project/Scripts/UI/Pause.cs
@@ -6,6 +6,13 @@ public class Pause : MonoBehaviour
 {
     public bool paused = false;
     GameController gameController;
+    public SpriteRenderer pauseFilter;
+
+    private void Start()
+    {
+        gameController = GetComponent<GameController>();
+        pauseFilter.enabled = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -13,14 +20,16 @@ public class Pause : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.P) && !paused)
         {
             Time.timeScale = 0f;
+            pauseFilter.enabled = true;
             paused = true;
             gameController.OnPaused(paused);
         }
         else if (Input.GetKeyDown(KeyCode.P) && paused)
         {
             Time.timeScale = 1f;
+            pauseFilter.enabled = false;
             paused = false;
-            gameController.OnPaused(paused);
+            gameController.OnPaused(paused); 
         }
     }
 }
